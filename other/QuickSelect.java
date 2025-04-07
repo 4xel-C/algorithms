b5b29432ec5a718698e22ea861d0158c393be0f1
Axel Cano <axel-cano@live.fr>
2025-04-07 21:47:15 +0200

    Optimization of quick select algorithm with Medianof3 and array randomization at the start. Correction of a bug trying to partition with equal low and high value, leading iteration trying to access elements out of bounds

--- other/QuickSelect.java
+++ other/QuickSelect.java
@@ -9,11 +9,16 @@ import utils.Helper;
 public class QuickSelect {
     
     public static int selectKSmallest(int[] array, int k) {
+
+        // shuffle the array to reduce the chance of having balanced parts
+        Helper.shuffle(array);
+
+
         k = k-1;
         int low = 0;  // Index of the first element
         int high = array.length - 1;  // Index of the last element
 
-        while (low <= high) {
+        while (low < high) {
             int j = partition(array, low, high);  // Partition using first element as the key, returning the good sorted position of the key
             
             // if j == k, because array[j] is in the correct sorted position, we can return j beeing, the jth smallest item of the array;
@@ -33,7 +38,10 @@ public class QuickSelect {
 
     public static int partition(int[] array, int low, int high) {
 
-        // Using the low postion as the key
+        // Using the low postion as the key (permutation with the median of 3)
+        int medianIndex = medianOf3(array, low, high);
+        Helper.permutation(array, medianIndex, low);
+
         int i = low + 1;
         int j = high;
         int key = array[low];
@@ -58,12 +66,32 @@ public class QuickSelect {
         return j;
     }
 
+    // Find the median of the three value (low, mid, hig) from an array to avoid having worst case in merge sort
+    public static int medianOf3(int[] array, int low, int high) {
+        int mid = (high + low) / 2;
+
+        // If low < mid < high or low > mid > high (mid median)
+        if ((array[low] - array[mid]) * (array[mid] - array[high]) >= 0) {
+            return mid;
+        
+            // if mid < low < high or mid > low > high (low median)
+        } else if ((array[low] - array[mid]) * (array[mid] - array[high]) <= 0) {
+            return low;
+
+            // if high index contains the median
+        } else {
+            return high;
+        }
+
+
+    }
+
     public static void main(String[] args) {
 
         int[] array = {100, 4, 9, 3, 2, 5, 8, 7, 10};
         Helper.printArray(array);
 
-        int k = selectKSmallest(array, 9); // Select the first element 
+        int k = selectKSmallest(array, 3); // Select the first element 
 
         System.out.println(k);
     }
